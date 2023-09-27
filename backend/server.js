c5bad1bc9e39c6aa9386e839ac73475ba480c39d
Yeoungmin Shin <yshin22@gmu.edu>
2023-09-26 20:06:08 -0400

    Redux implemented

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js'
-import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import productRoutes from './routes/productRoutes.js' 
 const port = process.env.PORT;
 
 connectDB(); // Connect to MongoDB
@@ -16,5 +17,8 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,45 +1,35 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
-import {Row, Col} from 'react-bootstrap'
-import Product from '../components/Product'
-// import products from '../products'
-import axios from 'axios'
+import React from 'react';
+import {Row, Col} from 'react-bootstrap';
+import Product from '../components/Product';
+import { useGetProductsQuery } from '../slices/productsApi';
+import Loader from '../components/Loader';
+import Message from '../components/Message'
+
 
   
 const HomeScreen = () => {
 
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const {data} = await axios.get('/api/products')
-
-      .catch(error => {
-        if (error.response) {
-          // The request was made, but the server responded with an error status code (e.g., 403)
-          console.error('Error status:', error.response.status);
-          console.error('Error data:', error.response.data);
-        } else {
-          // Something else went wrong
-          console.error('Error:', error.message);
-        }
-      });
-      setProducts(data);
-    };
-
-    fetchProducts();
-  }, []); 
+  const { data: products, isLoading, error } = useGetProductsQuery(); 
 
   return (
     <>
-      <h1>Latest products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product}/>
-          </Col>
-        ))}
-      </Row>
+      { isLoading ? (
+        <Loader/>) 
+        : error ? (
+          <Message variant='danger'>
+            {error?.data?.message || error.error}
+          </Message>
+          ) 
+        : (<>
+          <h1>Latest products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product}/>
+              </Col>
+            ))}
+          </Row>
+        </>) }  
     </>
    
   )
