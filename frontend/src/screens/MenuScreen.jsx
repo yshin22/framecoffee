6ccf1747754266de96d32e7439cb8b23bd2b7d01
Yeoungmin Shin <scarh54@gmail.com>
2023-10-31 22:24:51 -0400

    changes

--- frontend/src/screens/MenuScreen.jsx
+++ frontend/src/screens/MenuScreen.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import InstaFeeds from '../components/InstaFeeds'
 
 const MenuScreen = () => {
   return (
-    <div>MenuScreen</div>
+    <>
+      <InstaFeeds token={process.env.REACT_APP_INS_TOKEN} limit={12}/>
+    </>
   )
 }
 
