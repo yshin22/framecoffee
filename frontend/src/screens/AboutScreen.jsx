6697e87b01448a31e6348f0471eb9871e820961d
Yeoungmin Shin <scarh54@gmail.com>
2023-11-14 18:27:51 -0500

    about me layout

--- frontend/src/screens/AboutScreen.jsx
+++ frontend/src/screens/AboutScreen.jsx
@@ -3,23 +3,26 @@ import {Container, Row, Col} from 'react-bootstrap';
 import coffee from '../assets/gallery/frame_beans.jpg';
 import latte from '../assets/gallery/latte-art.jpeg';
 import woven from '../assets/gallery/hat_music.jpg';
+import Footer from '../components/Footer';
 
 const AboutScreen = () => {
   return (
     <>
-      <Row className='page-title'>
-        <h1>ABOUT US</h1>
-      </Row>
-      <Row className='sub-title py-2'>
-      <h2>Life happens, <span className='selectWord'>coffee</span> helps.</h2>
-      </Row>
+      <Container className='about-container'>
 
-      <Container>
+        <Row className='page-title'>
+          <h1>ABOUT US</h1>
+        </Row>
+
+        <Row className='sub-title py-2'>
+        <h2>Life happens, <span className='selectWord'>coffee</span> helps.</h2>
+        </Row>
+
         <Row className='image-row py-4'>
-          <Col className='img-container'>
+          <Col className='img-container' sm={true}>
             <img src={coffee} alt='coffee pour'/>
           </Col>
-          <Col>
+          <Col className='txt-container' sm={true}>
             <h1>
               <span className='selectWord'>Coffee</span> is one of the world’s most popular beverages. 
               Some claim it is the most widely consumed liquid in 
@@ -28,22 +31,22 @@ const AboutScreen = () => {
           </Col>
         </Row>
         <Row className='image-row py-4'>
-          <Col>
+          <Col xs={{span: 12, order:'2'}} sm={{span: 6, order:'1'}}>
             <h1>
             <span className='selectWord'>Coffee</span> is more than a beverage, however. It is a memory, 
             an anticipation, a lifetime of consoling moments of modest 
             pleasure woven into our lives.
             </h1>
           </Col>
-          <Col className='img-container'>
+          <Col className='img-container' xs={{span: 12, order: '1'}} sm={{span: 6, order: '2'}}>
             <img src={woven} alt=''/>
           </Col>
         </Row>
         <Row className='image-row py-4'>
-          <Col className='img-container'>
+          <Col className='img-container' sm={true}>
             <img src={latte} alt=''/>
           </Col>
-          <Col>
+          <Col sm={true}>
             <h1>
             Having a good cup of <span className='selectWord'>coffee</span> in a good place, that is what 
             we love to share with you.
@@ -51,6 +54,8 @@ const AboutScreen = () => {
           </Col>
         </Row>
       </Container>
+      <Footer/>
+
     </>
   )
 }
