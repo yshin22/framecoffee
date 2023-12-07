a78845d3eb9874baca0b9ebda9dbffb0d3a60903
Yeoungmin Shin <scarh54@gmail.com>
2023-12-06 23:39:43 -0500

    video changes

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -14,7 +14,7 @@ const Feed = (props) => {
                         height='auto' 
                         src={media_url} 
                         type="video/mp4" 
-                        controls playsinline>
+                        autoPlay muted>
                     </video>
                 </a>
 
