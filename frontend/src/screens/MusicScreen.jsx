cafce1d362f92b06a2db91a7aa89648259d0ce74
Yeoungmin Shin <yshin22@gmu.edu>
2023-12-08 13:56:10 -0500

    apple music player

--- frontend/src/screens/MusicScreen.jsx
+++ frontend/src/screens/MusicScreen.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import '../assets/styles/screens/musicscreen.css'
+import {Helmet} from 'react-helmet';
+import { useEffect } from 'react';
+import { Container, Row} from 'react-bootstrap';
+import Footer from '../components/Footer';
 
 const MusicScreen = () => {
+  
   return (
-    <div>MusicScreen</div>
+    <>
+    <Container className='musicScreen-container'>
+      <Row className='page-title'>
+        <h1>MUSIC</h1>
+      </Row>
+
+      <Row className='sub-title py-2'>
+          <h2><span className='selectWord'>MUSIC</span>  IS THE UNIVERSIAL LANGUAGE OF MANKIND</h2>
+      </Row>
+
+      <div className='music-player-container'>
+
+        <iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="450" style={{
+          width:"100%", maxWidth: "660px", overflow: "hidden", background:"transparent"
+        }} sandbox="allow-forms allow-popups allow-same-origin allow-scripts 
+        allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" 
+        src="https://embed.music.apple.com/us/album/this-thing-called-living-ep/1543015873"></iframe>
+
+      </div>
+    </Container>
+    <Footer/>
+    </>
+    
   )
 }
 
