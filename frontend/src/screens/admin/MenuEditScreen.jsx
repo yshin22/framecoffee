8dc8c7874728e528e487c455ef26c555acfcb449
Yeoungmin Shin <yshin22@gmu.edu>
2024-01-08 15:16:01 -0500

    menu edit screen UI update

--- frontend/src/screens/admin/MenuEditScreen.jsx
+++ frontend/src/screens/admin/MenuEditScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Form, Button, Row, Col} from 'react-bootstrap';
+import {Form, Button, Container, Row, Col, Stack} from 'react-bootstrap';
 import {
     useUploadMenuImageMutation,
     useGetMenuImagesQuery,
@@ -49,31 +49,36 @@ const MenuEditScreen = () => {
       }, [file]);
 
   return (
-    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100vh'}}>
-        <input type='file' onChange={e => setFile(e.target.files)} multiple/>
-        <Row>
-          {filesArray?.map((f) => (
-            <Row>
-              {console.log(f)}
-              {f.name}
-            </Row>
-          ))}
-        </Row>
-        <button onClick={uploadFileHandler}>Upload</button>
+    <>
+      <Container className='menuEdit-container'>
+        <Stack gap={3} className='mx-5'>
+          <div className='file-img-container'>
+            {image?.map((i) => (
+              <div className='file-img'>
+                {/* <img style={{height: '300px', width: '250px'}}src={`/uploads/` + i} alt="menu"/> */}
+                <img style={{height: '300px', width: '250px'}}src={`http://localhost:4000/uploads/` + i} alt="menu"/>
 
-        <Row>
-          {image?.map((i) => (
-            <Col>
-              <img style={{height: '300px', width: '250px'}}src={`/uploads/` + i} alt="menu"/>
-              {/* <img style={{height: '300px', width: '250px'}}src={`http://localhost:4000/uploads/` + i} alt="menu"/> */}
-
-            </Col>
-          ))}
-          <Col>
-          
-          </Col>
-        </Row>
-    </div>
+              </div>
+            ))}
+          </div>
+          <div className='file-btn-container'>
+            <input type='file' placeholder='Upload' onChange={e => setFile(e.target.files)} multiple/>
+          </div>
+          <div className='file-name-container'>
+          <Stack gap={2} className='file-name-wrapper'>
+            {filesArray?.map((f) => (
+              <div className='file-names'>
+                {console.log(f)}
+                {f.name}
+              </div>
+            ))}
+          </Stack>
+          </div>
+          <button className='file-upload-btn'onClick={uploadFileHandler}>Upload</button>
+        </Stack>
+        
+      </Container>
+    </>
   )
 }
 
